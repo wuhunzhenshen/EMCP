5f7829cf3c3e14b845dd96e5175edd784f6a0638
wuhunzhenshen <452671154@qq.com>
2022-01-22 20:17:26 +0800

    Signed-off-by: wuhunzhenshen <452671154@qq.com>

---
+++ new3.py
@@ -0,0 +1,29 @@
+﻿from selenium import webdriver
+import time
+import os
+
+driver=webdriver.Chrome()
+
+driver.get("http://new.lfemcp.com/login")
+time.sleep(0.2)
+driver.maximize_window()
+#登录
+driver.find_element_by_xpath('//*[@id="app"]/div/div[2]/div/form/div[2]/div/div/input').clear()
+driver.find_element_by_xpath('//*[@id="app"]/div/div[2]/div/form/div[2]/div/div/input').send_keys("wb5")
+driver.find_element_by_xpath('//*[@id="app"]/div/div[2]/div/form/div[3]/div/div/input').clear()
+driver.find_element_by_xpath('//*[@id="app"]/div/div[2]/div/form/div[3]/div/div/input').send_keys("11111111")
+time.sleep(1)
+driver.find_element_by_xpath('//*[@id="app"]/div/div[2]/div/form/button').click()
+time.sleep(1)
+
+
+
+
+
+
+
+
+
+
+
+
