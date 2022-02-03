b8a916939eca2f298e1997958d68449e2a2df1ba
wuhunzhenshen <452671154@qq.com>
2022-02-03 20:13:18 +0800

    GIT

--- page/zutaimange_page.py
+++ page/zutaimange_page.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 import time
+from selenium.webdriver.common.keys import Keys
 
 
 class ZutaimangePage:
@@ -7,38 +8,43 @@ class ZutaimangePage:
     def __init__(self, driver):
         self.driver = driver
 
+    # 点击保存
+    def slave_button(self):
+        time.sleep(1)
+        self.driver.find_element(By.XPATH, '//*[@id="app"]/section/header/div/div[4]/a[2]').click()
+        time.sleep(1)
+
     # 背景色
     def backcolor_button(self):
         time.sleep(1)
-
         self.driver.find_element(By.XPATH, '//*[@id="formBar"]/aside[1]/div/div[2]/div/div[1]/div/div[1]').click()
         time.sleep(1)
 
     # 背景色红色
     def backcolor_red_button(self):
-        time.sleep(1)
+        time.sleep(0.3)
         self.driver.find_element(By.XPATH, '/html/body/div[3]/div/div[5]/div/div/div[21]/div').click()
 
     # 背景色蓝色
     def backcolor_blue_button(self):
-        time.sleep(1)
+        time.sleep(0.3)
         self.driver.find_element(By.XPATH, '/html/body/div[3]/div/div[5]/div/div/div[14]/div').click()
 
     # 背景色橙色
     def backcolor_orange_button(self):
-        time.sleep(1)
+        time.sleep(0.3)
         self.driver.find_element(By.XPATH, '/html/body/div[3]/div/div[5]/div/div/div[28]/div').click()
 
     # 背景色透明度
     def backcolor_trans(self, trans):
-        time.sleep(2)
+        time.sleep(0.8)
         self.driver.find_element(By.XPATH, '/html/body/div[3]/div/div[1]/div[5]/input').clear()
-        time.sleep(1)
+        time.sleep(0.5)
         self.driver.find_element(By.XPATH, '/html/body/div[3]/div/div[1]/div[5]/input').send_keys(trans)
 
     # 背景色透明度查询
     def backcolor_trans2(self):
-        time.sleep(2)
+        time.sleep(1)
         var = self.driver.find_element(By.XPATH, '/html/body/div[3]/div/div[1]/div[5]/input')
         return var
 
@@ -51,3 +57,68 @@ class ZutaimangePage:
     def backcolor_close_button(self):
         time.sleep(1)
         self.driver.find_element(By.XPATH, '/html/body/div[3]/div/h4/i').click()
+
+    # 勾选自定义分辨率按钮
+    def custom_resolution_button(self):
+        time.sleep(0.3)
+        self.driver.find_element(By.XPATH, '//*[@id="formBar"]/aside[1]/div/div[2]/'
+                                           'div/div[2]/div[3]/div[1]/label/span[1]').click()
+        time.sleep(1)
+
+    # 输入自定义分辨率宽度和高度
+    def custom_resolution_width_height(self, width, height):
+        time.sleep(0.1)
+        # 清空宽和高
+        for i in range(5):
+            self.driver.find_element(By.XPATH, '//*[@id="formBar"]/aside[1]/div/div[2]/'
+                                               'div/div[2]/div[3]/div[2]/div[2]/div/div/input').send_keys(
+                Keys.BACKSPACE)
+
+        time.sleep(1)
+        # 设置宽
+        self.driver.find_element(By.XPATH, '//*[@id="formBar"]/aside[1]/div/div[2]/'
+                                           'div/div[2]/div[3]/div[2]/div[2]/div/div/input').send_keys(width)
+
+        for j in range(5):
+            self.driver.find_element(By.XPATH, '//*[@id="formBar"]/aside[1]/div/div[2]/'
+                                               'div/div[2]/div[3]/div[2]/div[5]/div/div/input').send_keys(
+                Keys.BACKSPACE)
+        # 设置高
+        time.sleep(1)
+        self.driver.find_element(By.XPATH, '//*[@id="formBar"]/aside[1]/div/div[2]/'
+                                           'div/div[2]/div[3]/div[2]/div[5]/div/div/input').send_keys(height)
+
+    # 读取宽
+    def resolution_width(self):
+
+        var = self.driver.find_element(By.XPATH, '//*[@id="formBar"]/aside[1]/div/div[2]/'
+                                                 'div/div[2]/div[3]/div[2]/'
+                                                 'div[2]/div/div/input').get_attribute("value")
+        return var
+
+    # 读取高
+    def resolution_height(self):
+
+        var = self.driver.find_element(By.XPATH, '//*[@id="formBar"]/aside[1]/div/div[2]/'
+                                                 'div/div[2]/div[3]/div[2]/div[5]/div/div/'
+                                                 'input').get_attribute("value")
+        return var
+
+    # 设置页面密码
+    def set_page_key(self, key2):
+        self.driver.find_element(By.XPATH, '//*[@id="formBar"]/aside[1]/div/div[2]/'
+                                           'div/div[3]/div[2]/div[1]/label/span[1]').click()
+        time.sleep(1)
+        self.driver.find_element(By.XPATH, '//*[@id="formBar"]/aside[1]/div/div[2]/'
+                                           'div/div[3]/div[2]/div[2]/form/div/div/div/input').clear()
+
+        time.sleep(1)
+        self.driver.find_element(By.XPATH, '//*[@id="formBar"]/aside[1]/div/div[2]/'
+                                           'div/div[3]/div[2]/div[2]/form/div/div/div/input').send_keys(key2)
+
+    # 读取页面密码
+    def read_page_key(self):
+        var = self.driver.find_element(By.XPATH, '//*[@id="formBar"]/aside[1]/div/div[2]/'
+                                                 'div/div[3]/div[2]/div[2]/form/div/div/'
+                                                 'div/input').get_attribute("value")
+        return var
--- test_case/test_data_center.py
+++ test_case/test_data_center.py
@@ -40,7 +40,7 @@ class Test_data_center(unittest.TestCase):
         self.login(name, password)
         lg = LoginPage(self.driver)
         mingzi = lg.login_name()
-        print(" 用户: " + mingzi + " 登录成功")
+        print("用户: " + mingzi + " 登录成功")
         self.assertIn(mingzi, name)
         time.sleep(0.3)
 
@@ -53,7 +53,7 @@ class Test_data_center(unittest.TestCase):
         mange.management_button()
 
         # 点击数据中心
-        time.sleep(1)
+        time.sleep(0.3)
         data_center = LeftPage(self.driver)
         time.sleep(0.3)
         data_center.data_center_mange_button()
@@ -61,7 +61,7 @@ class Test_data_center(unittest.TestCase):
         data_center.data_center_zutai_mange_button()
 
         # 关闭引导页
-        time.sleep(2)
+        time.sleep(0.6)
         data_center.data_center_leader()
 
     def test_3_backcolor_mange(self):
@@ -73,24 +73,52 @@ class Test_data_center(unittest.TestCase):
         time.sleep(2)
         # 选择背景颜色为橙色
         zutai.backcolor_red_button()
-        time.sleep(0.3)
+        time.sleep(0.2)
         zutai.backcolor_blue_button()
-        time.sleep(0.3)
+        time.sleep(0.2)
         zutai.backcolor_orange_button()
-        time.sleep(0.3)
+        time.sleep(0.2)
         # 设置背景色透明度
-        zutai.backcolor_trans("0.3")
-        time.sleep(0.3)
+        zutai.backcolor_trans("0.2")
+        time.sleep(0.2)
         zutai.backcolor_trans("0.5")
-        time.sleep(0.3)
+        time.sleep(0.2)
         zutai.backcolor_trans("0.9")
         print("背景透明度为： ", zutai.backcolor_trans2().get_attribute("value"))
         # 判断背景颜色
         hex2 = zutai.backcolor_hex().get_attribute("value")
         self.assertIn("FF8D02", hex2)
         print("背景HEX颜色为： ", hex2)
+
         # 关闭背景色设置弹窗
         zutai.backcolor_close_button()
+        time.sleep(0.6)
+        # 勾选自定义分辨率
+        zutai.custom_resolution_button()
+        time.sleep(1)
+        # 设置宽和高
+        width = '1920'
+        height = '1080'
+        zutai.custom_resolution_width_height(width, height)
+        # 保存
+        time.sleep(1)
+        zutai.slave_button()
+        # 断言宽和高设置成功与否
+        wi = zutai.resolution_width()
+        he = zutai.resolution_height()
+        self.assertIn(wi, width)
+        self.assertIn(he, height)
+        if wi == width and he == height:
+            print("分辨率设置成功！宽为：" + wi, "高为：" + he)
+        else:
+            print("分辨率设置失败！")
+        # 设置页面密码
+        keys3 = "123456"
+        zutai.set_page_key(keys3)
+        zutai.slave_button()
+        # 判断页面密码是否设置成功
+        pagekeys = zutai.read_page_key()
+        self.assertIn(keys3, pagekeys)
 
     # 关闭浏览器
     @classmethod
@@ -100,11 +128,10 @@ class Test_data_center(unittest.TestCase):
         time.sleep(2)
         cls.driver.quit()
 
-
-if __name__ == '__main__':
-    # suits = unittest.TestSuite()
-    # suits.addTest(Test_data_center("test_1_login"))
-    # suits.addTest(Test_data_center("test_2_data_center_mange"))
-    # runner = unittest.TextTestRunner()
-    # runner.run(suits)
-    unittest.main(verbosity=2)
+# if __name__ == '__main__':
+# suits = unittest.TestSuite()
+# suits.addTest(Test_data_center("test_1_login"))
+# suits.addTest(Test_data_center("test_2_data_center_mange"))
+# runner = unittest.TextTestRunner()
+# runner.run(suits)
+# unittest.main(verbosity=2)
